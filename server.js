f87c68accf15b02908c69fccdb8b42503ea45dfe
James <jhresource3@gmail.com>
2016-08-14 11:36:27 -0700

    added createOrder

--- server.js
+++ server.js
@@ -17,18 +17,28 @@ app.get('/validateUser',function (req,res) {
   var password = req.param('password');
   var validated = 0;
 
-//  fs.readFile(__dirname + "/" + "users.json",'utf8', function (err,data) {
-//    obj = JSON.parse(data);
-//    validated = obj[0].username == username;
   if ((username == "james") && (password == "one")) {
       validated = 1;
     } else {
     validated = 0;
   }
 
-res.send(validated.toString())
+  res.send(validated.toString())
 
 });
+
+app.get('/createOrder', function (req,res) {
+  var userId = req.param('userId');
+  var pickupDateTime = req.param('pickupDateTime');
+  var deliveryDateTime = req.param('deliveryDateTime');
+
+  log = "user: " + userId + " placed an order for pickup on " + pickupDateTime + " and delivery on " + deliveryDateTime;
+
+  console.log(log);
+  res.send('Order created');
+
+});
+
 
 
 var server = app.listen(8081, function () {
